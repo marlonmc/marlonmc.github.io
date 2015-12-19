a503b98a829e40a9aa1ea97180cc60c53b7045ef
marlonmc <marlon.m.clark08@gmail.com>
2015-12-19 11:08:21 -0600

    Added bfresh link; photo books for band and flyers

---
+++ js/slickPhoto.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.slickPersonal').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        centerMode: true,
+        variableWidth: true,
+        arrows: false
+    });
+    
+    $('.photoCarousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        centerMode: true,
+        variableWidth: true,
+        arrows: false
+    });
+});
